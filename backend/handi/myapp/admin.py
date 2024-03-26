45de38853937a2c292091f827e6d607bc14030d7
otnick <116823292+otnick@users.noreply.github.com>
2024-03-26 16:21:24 +0100

    models

--- backend/handi/myapp/admin.py
+++ backend/handi/myapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Liga, Spieler
 
 # Register your models here.
+admin.site.register(Liga)
+admin.site.register(Spieler)
--- backend/handi/myapp/models.py
+++ backend/handi/myapp/models.py
@@ -4,14 +4,14 @@ class Spieler(models.Model):
     name = models.CharField(max_length=100)
     titel = models.IntegerField(default=0)
     auto = models.CharField(max_length=100)
-    punkte = models.IntegerField(default=0, min_value=0)
+    punkte = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
 
 class Liga(models.Model):
     name = models.CharField(max_length=100)
-    spieltage = models.IntegerField(default=0, min_value=0)
+    spieltage = models.IntegerField(default=0)
     spieler = models.ManyToManyField(Spieler)
 
     def __str__(self):
