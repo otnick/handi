367a584bdff0cd5b08b4bcb032dd877b002d9f66
otnick <116823292+otnick@users.noreply.github.com>
2024-03-26 15:24:36 +0100

    backend

---
+++ backend/handi/myapp/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Spieler(models.Model):
+    name = models.CharField(max_length=100)
+    titel = models.IntegerField(default=0)
+    auto = models.CharField(max_length=100)
+    punkte = models.IntegerField(default=0, min_value=0)
+
+    def __str__(self):
+        return self.name
+
+class Liga(models.Model):
+    name = models.CharField(max_length=100)
+    spieltage = models.IntegerField(default=0, min_value=0)
+    spieler = models.ManyToManyField(Spieler)
+
+    def __str__(self):
+        return self.name
--- my-skeleton-app/src/routes/raketenliga/+page.svelte
+++ my-skeleton-app/src/routes/raketenliga/+page.svelte
@@ -17,13 +17,13 @@
 </script>
 
 <section class="img-bg mt-28"/>
-<div class="grid grid-cols-6 gap-4">
-    <h2 class="h2 font-bold">Raketenligen</h2>
+<div class="grid grid-cols-4 gap-4 justify-items-center mt-48">
+    <h2 class="h2 font-bold col-span-4 mb-10">Raketenligen</h2>
     {#each leagues as league}
-    <a class="card p-4" href="/">{league}</a>
+    <a class="card p-4" href="/raketenliga/{league}">{league}</a>
     {/each}
     <button
-        class="btn variant-filled col-start-3 col-end-5"
+        class="btn variant-filled col-start-2 col-end-4 mt-10"
         on:click={addLeague}
         >
         new league
@@ -63,5 +63,18 @@
         50% {
             transform: translate(-50%, -80%) scale(1.5);
         }
+    }
+
+    .card {
+        @apply w-96 p-4 text-center rounded-lg shadow-md;
+        background-color: var(--color-primary-400);
+        color: var(--color-primary-50);
+    }
+    .card:hover {
+        @apply bg-primary-500;
+    }
+
+    .btn {
+        @apply w-1/4 p-4 text-center rounded-lg shadow-md;
     }
     </style>
