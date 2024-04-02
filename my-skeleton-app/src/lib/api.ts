e24693b9a7624d0e7b1333bbd951f579296a5314
otnick <116823292+otnick@users.noreply.github.com>
2024-04-02 13:43:35 +0200

    connect backend to backendserver

--- my-skeleton-app/src/lib/api.ts
+++ my-skeleton-app/src/lib/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://127.0.0.1:8000/api/';
+const BASE_URL = 'https://nickot.pythonanywhere.com/api/';
 const FISCHSTATS_URL = [
     "fischart",
     "angler",
--- my-skeleton-app/src/lib/apiconnector/FischartConnector.ts
+++ my-skeleton-app/src/lib/apiconnector/FischartConnector.ts
@@ -1,17 +1,14 @@
-import {Fischart} from "$lib/types"
+// import type { IFischart } from "$lib/types";
 
-class Fischart {
-    // Define the properties of the Fischart object here
-}
+// class Fischart implements IFischart {
+    
+// }
 
-function getFischartList(): Fischart[] {
-    // Implement the logic to retrieve the list of Fischart objects here
-    // For example, you can make an API call or fetch data from a database
 
-    // Return a list of Fischart objects
-    return [
-        new Fischart(/* pass the required properties here */),
-        new Fischart(/* pass the required properties here */),
-        // Add more Fischart objects as needed
-    ];
-}
+// function getFischartList(): IFischart[] {
+//     // Implement the logic to retrieve the list of Fischart objects here
+//     // For example, you can make an API call or fetch data from a database
+
+//     // Return a list of Fischart objects
+    
+// }
