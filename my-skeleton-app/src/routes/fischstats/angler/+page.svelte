6608b2c2f861c7d275b5fa3bd60ad05d1bfd1d9b
otnick <116823292+otnick@users.noreply.github.com>
2024-05-01 18:29:07 +0200

    starting scene

--- my-skeleton-app/src/routes/fischstats/angler/+page.svelte
+++ my-skeleton-app/src/routes/fischstats/angler/+page.svelte
@@ -1,1 +1,8 @@
+<script lang="ts">
+    import Animation from "$lib/Components/Animation.svelte";
+	import Backlight from "$lib/Components/Backlight.svelte";
+</script>
+
+<Backlight/>
+<Animation/>
 Alle Angler
